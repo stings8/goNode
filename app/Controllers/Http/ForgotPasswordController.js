1cd48212f2577866a64ce3c7c6b89b8a4f971d04
stings8 <stingufc@gmail.com>
2020-03-10 08:17:32 -0300

    create forgotpassword

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,7 +1,39 @@
 'use strict'
 
+const crypto = require('crypto')
+const User = use('App/Models/User')
+const Mail = use('Mail')
+
 class ForgotPasswordController {
-  async store () {}
+  async store ({ request, response }) {
+    try {
+      const email = request.input('email')
+      const user = await User.findByOrFail('email', email)
+
+      user.token = crypto.randomBytes(10).toString('hex')
+      user.token_created_at = new Date()
+
+      await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.mail)
+            .from('sting@gmail.com', 'Sting')
+            .subject('Recuperar senha')
+        })
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Algo não deu certo' } })
+    }
+  }
 }
 
 module.exports = ForgotPasswordController
